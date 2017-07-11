d57771f97ab0d836d724cfdcfe1a19fd2f26adc7
Zola <zolamcadie@gmail.com>
2017-07-11 20:50:26 +0000

    implemented character counter on new tweet form

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+
+  $(".new-tweet textarea").on("keyup", function () {
+    var text = $(this).val();
+    var charLeft = 140 - (text.length);
+    var counter = $(this).parent().find(".counter")
+    counter.text(charLeft);
+    if (charLeft < 0) {
+       counter.addClass("invalid");
+    } else {
+      counter.removeClass("invalid");
+    }
+  });
+
+});
