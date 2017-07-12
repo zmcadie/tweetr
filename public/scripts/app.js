9eb3bdca72652418589ee92f33b7ecf9f7e348ea
Zola <zolamcadie@gmail.com>
2017-07-12 02:02:25 +0000

    refactored tweet element builder for readability, implemented render tweet function to display tweet database to index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,53 @@
 $(document).ready(function () {
-  var tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+
+///////////////////////////////////////////
+// functions for building tweet element, //
+// seperated from main to increase       //
+// readability                           //
+///////////////////////////////////////////
+  function createTweetHeader(tweetData) {
+    var $header = $("<header>")
+      .append($("<img class='avatar' src='" + tweetData.user.avatars.small + "'>"))
+      .append($("<div class='username'>").text(tweetData.user.name))
+      .append($("<div class='handle'>").text(tweetData.user.handle));
+    return $header;
   }
 
-  function createTweetElement(tweetData) {
-    var $tweet = $("<article>").addClass("tweet")
-      .append($("<header>")
-        .append($("<img class='avatar' src='" + tweetData.user.avatars.small + "'>"))
-        .append($("<div class='username'>").text(tweetData.user.name))
-        .append($("<div class='handle'>").text(tweetData.user.handle))
-      ).append($("<section>")
-        .text(tweetData.content.text)
-      ).append($("<footer>")
-        .text("Created: " + tweetData.created_at)
-        .append($("<span class='icons'>")
-          .append($("<img src='/images/flag.png'>"))
-          .append($("<img src='/images/arrows.png'>"))
-          .append($("<img src='/images/like.png'>"))
-        )
+  function createTweetBody(tweetData) {
+    var $body = $("<section>")
+      .text(tweetData.content.text);
+    return $body;
+  }
+
+  function createTweetFooter(tweetData) {
+    var $footer = $("<footer>")
+      .text("Created: " + tweetData.created_at)
+      .append($("<span class='icons'>")
+        .append($("<img src='/images/flag.png'>"))
+        .append($("<img src='/images/arrows.png'>"))
+        .append($("<img src='/images/like.png'>"))
       );
+    return $footer;
+  }
+
+// builds the actual tweet using above functions
+  function createTweetElement(tweetData) {
+    var $tweet = $("<article class='tweet'>")
+      .append(createTweetHeader(tweetData))
+      .append(createTweetBody(tweetData))
+      .append(createTweetFooter(tweetData));
     return $tweet;
   };
 
-  var $tweet = createTweetElement(tweetData);
+//////////////////////////////////////
+// takes an array of tweet objects, //
+// builds element for each          //
+//////////////////////////////////////
+  function renderTweets(tweets) {
+    tweets.forEach(function (tweetObj) {
+      var tweet = createTweetElement(tweetObj);
+      $("#tweets-container").append(tweet);
+    })
+  }
 
-  console.log($tweet);
-
-  $('#tweets-container').append($tweet);
 });
